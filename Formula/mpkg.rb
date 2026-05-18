656d835e7673fb5fdad0df9521ac5f2c9c2b2d1a
Ahmad Rifa'i <personal.rifai@gmail.com>
2026-05-19 00:00:41 +0700

    chore(mpkg): fix formula style to follow Homebrew conventions

--- Formula/mpkg.rb
+++ Formula/mpkg.rb
@@ -1,5 +1,5 @@
 class Mpkg < Formula
-  desc "Generate static HTML for Go vanity import paths."
+  desc "Generate static HTML for Go vanity import paths"
   homepage "https://github.com/sembraniteam/mpkg"
   url "https://github.com/sembraniteam/mpkg/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "7421266d9b088f33d7a4d93707406983f9f52e09b838ab6a34b75faeb98b3ae1"
@@ -7,17 +7,17 @@ class Mpkg < Formula
 
   depends_on "go" => :build
 
-  def install
-    system "go", "build", "-o", bin/"mpkg", "."
-  end
-
   livecheck do
     url :url
     regex(/^v?(\d+(?:\.\d+)+)$/i)
     strategy :github_latest
   end
 
+  def install
+    system "go", "build", "-o", bin/"mpkg", "."
+  end
+
   test do
-    system "#{bin}/mpkg", "--help"
+    system bin/"mpkg", "--help"
   end
 end
