651154907b0ab23576ef6734e1cdab3ecc1ccc51
Ahmad Rifa'i <personal.rifai@gmail.com>
2026-05-19 09:39:06 +0700

    chore: bump mpkg to v0.2.0 and update test command

--- Formula/mpkg.rb
+++ Formula/mpkg.rb
@@ -1,8 +1,8 @@
 class Mpkg < Formula
   desc "Generate static HTML for Go vanity import paths"
   homepage "https://github.com/sembraniteam/mpkg"
-  url "https://github.com/sembraniteam/mpkg/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "7421266d9b088f33d7a4d93707406983f9f52e09b838ab6a34b75faeb98b3ae1"
+  url "https://github.com/sembraniteam/mpkg/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "715ce05f2b8064ed7b18455fb94dc5344b43df0b275770d57b037e2ed8d19e03"
   license "MIT"
 
   livecheck do
@@ -18,6 +18,6 @@ class Mpkg < Formula
   end
 
   test do
-    system bin/"mpkg", "--help"
+    system bin/"mpkg", "version"
   end
 end
